eb81f59490540055815871b64626e1d74e1131dc
mellonnen <arvidgotthard98@gmail.com>
2021-04-13 10:24:38 +0200

    parsing with gjson instead

--- pkg/data/parsing.go
+++ pkg/data/parsing.go
@@ -1,38 +1,24 @@
 package data
 
 import (
-	"encoding/json"
 	"strings"
 
 	mapset "github.com/deckarep/golang-set"
+	"github.com/tidwall/gjson"
 )
 
 func ParsePackage(packageData []byte) (mapset.Set, error) {
 	dependencyCollector := mapset.NewSet()
-
-	var v interface{}
-	err := json.Unmarshal(packageData, &v)
-	if err != nil {
-		return dependencyCollector, err
-	}
-	data := v.(map[string]interface{})
-
-	devDependencies, found := data["devDependencies"]
-	if found {
-		for dependency := range devDependencies.(map[string]interface{}) {
-			// replace @  and / with hexadecimal counterparts
-			formatted := strings.ReplaceAll(strings.ReplaceAll(dependency, "@", "%40"), "/", "%2f")
+	
+	results := gjson.GetManyBytes(packageData, "devDependencies", "dependencies")
+	
+	for _, result := range results {
+		result.ForEach(func(key, value gjson.Result) bool {
+			formatted := strings.ReplaceAll(strings.ReplaceAll(key.String(), "@", "%40"), "/", "%2f")
 			dependencyCollector.Add(formatted)
-		}
-	}
-
-	dependencies, found := data["dependencies"]
-	if found {
-		for dependency := range dependencies.(map[string]interface{}) {
-			formatted := strings.ReplaceAll(strings.ReplaceAll(dependency, "@", "%40"), "/", "%2f")
-			dependencyCollector.Add(formatted)
-		}
-	}
-
+			return true
+		}) 
+	} 	
+	
 	return dependencyCollector, nil
 }
