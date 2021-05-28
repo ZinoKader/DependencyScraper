b552faa9a43322d5a81159cd1f4dd60dd11a95da
Zino Kader <zinokad@gmail.com>
2021-05-28 23:43:04 +0200

    Update parsing.go

--- pkg/data/parsing.go
+++ pkg/data/parsing.go
@@ -9,21 +9,17 @@ import (
 
 func ParsePackage(packageData []byte) (mapset.Set, error) {
 	dependencyCollector := mapset.NewSet()
-<<<<<<< HEAD
 
 	results := gjson.GetManyBytes(packageData, "devDependencies", "dependencies")
 
 	for _, result := range results {
 		result.ForEach(func(key, value gjson.Result) bool {
-			formatted := strings.ReplaceAll(strings.ReplaceAll(key.String(), "@",
-				"%40"), "/", "%2f")
+			formatted := strings.ReplaceAll(strings.ReplaceAll(key.String(), "@", "%40"), "/", "%2f")
 			dependencyCollector.Add(formatted)
 			return true
 		})
 	}
 
-=======
-	
 	results := gjson.GetManyBytes(packageData, "devDependencies", "dependencies")
 	
 	for _, result := range results {
@@ -34,6 +30,5 @@ func ParsePackage(packageData []byte) (mapset.Set, error) {
 		}) 
 	} 	
 	
->>>>>>> eb81f59490540055815871b64626e1d74e1131dc
 	return dependencyCollector, nil
 }
